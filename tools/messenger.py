d56d860eb06d0e7d57a63e7ed669382b194b8b85
Reagan <rrr.chanhiulok@gmail.com>
2024-10-06 14:18:50 +0000

    add comments

--- tools/messenger.py
+++ tools/messenger.py
@@ -1,13 +1,21 @@
 import re
 
+from telegram import Update
+
 from datasources.faiss_ds import FAISSDS
 from db.db import db
 
 
 async def schola_reply(
-    update, message, reply_markup=None, parse_mode="HTML", *args, **kwargs
+    update: Update,
+    message: str,
+    reply_markup: bool = None,
+    parse_mode: str = "HTML",
+    *args,
+    **kwargs,
 ) -> None:
     """Create a telegram text reply, given formatting option"""
+
     try:
         # remove code blocks around
         code_fence_pattern = r"^```(?:json)?\s*([\s\S]*?)\s*```$"
@@ -16,6 +24,9 @@ async def schola_reply(
             message = match.group(1).strip()
         else:
             message = message.strip("`")
+
+        # turn **text** markdowns into HTML format <b>text</b>
+        message = re.sub(r"\*\*([^\*]{1,23}?)\*\*", r"<b>\1</b>", message)
 
         # send text reply
         await update.message.reply_text(
@@ -36,14 +47,17 @@ def retrieve_from_subject(query: str, subject: str, topk: int = 5) -> str:
 
     info = db.get_subject_info_by_subject_name(subject)
 
+    # check if the subject the user enrolled to has an entry in the DB, if not return ""
     if not info:
         return ""
 
     ds: bool = dict(info).get("use_datasource", False)
 
+    # the subject has an entry, check if it uses any datasource files, if not return ""
     if not ds:
         return ""
 
+    # perform a search on its datasource, and return the search results.
     faiss_ds = FAISSDS(index_name=subject)
     hits = faiss_ds.search_request(query, topk=topk)
     res = ""
--- tools/whisper.py
+++ tools/whisper.py
@@ -4,6 +4,7 @@ import openai
 def transcribe_voice(file_path: str) -> str:
     """
     Transcribe the voice file to text using OpenAI's Whisper API.
+    Able to automatically detect language and output string accordingly.
 
     Args:
         file_path (str): The path to the voice file.
@@ -12,6 +13,7 @@ def transcribe_voice(file_path: str) -> str:
         str: The transcribed text.
     """
     client = openai.OpenAI()
+
     with open(file_path, "rb") as audio_file:
         transcript = client.audio.transcriptions.create(
             model="whisper-1", file=audio_file
